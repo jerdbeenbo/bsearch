3d87209fae0e4979fb1cf26ad331ef5992fd4f92
Jarred Jenkins <jarredjenkins1000@gmail.com>
2025-02-25 12:23:11 +1000

    fixed warning and added computation clock

--- bsnew.c
+++ bsnew.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
-
-void main() {
-    
-    int data[10] = {1, 3, 4, 5, 6, 7, 8, 10, 11, 12};
-    int target = 10;
-
-    printf("Target %d found at index %d\n", target, bsearch(data, 10, target));
-}
+#include <time.h>
 
 int bsearch(int data[], int arrLength, int target) {
 
@@ -35,4 +28,16 @@ int bsearch(int data[], int arrLength, int target) {
 
     //target value is not in array
     return -1;
+}
+
+void main() {
+    
+    int data[10] = {1, 3, 4, 5, 6, 7, 8, 10, 11, 12};
+    int target = 10;
+
+    clock_t begin = clock();
+    printf("Target %d found at index %d\n", target, bsearch(data, 10, target));
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Computation time: %f\n", time_spent);
 }
